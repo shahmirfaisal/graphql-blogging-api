d3a1a5e9b461a14981ffd397d04d798e6cd49ef6
Shahmir Faisal <shahmir049@gmail.com>
2020-09-07 13:42:42 +0500

    Added .env file

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -1,10 +1,12 @@
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({ path: "config/prod.env" });
 
 const getUserId = ({ request }, requireAuth = true) => {
   const header = request.headers.authorization;
   if (!header && requireAuth) throw new Error("Authentication required!");
   const token = header.replace("Bearer ", "");
-  const { userId } = jwt.verify(token, "programmingchola");
+  const { userId } = jwt.verify(token, process.env.SECRET);
   return userId;
 };
 
